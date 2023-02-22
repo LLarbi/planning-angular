234a7c16646ef222f9627644147cc089ba51fa67
Jat15 <jinan15@hotmail.com>
2023-02-22 12:17:31 +0100

    [Component] Create Auth

---
+++ src/app/planning/pages/auth/auth.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {AuthService} from "../../../core/auth/auth.service";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+
+export class AuthComponent {
+  email!: string;
+  password!: string;
+
+  constructor(
+    private authService: AuthService
+  ) {
+  }
+
+  onSubmit() {
+    this.authService.post(this.email, this.password).subscribe(
+      {
+        next: (value: Event) => console.log(value),
+        error: (error: string) => console.log(error),
+        complete: () => console.log("fini")});
+  }
+}

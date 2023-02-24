0408363b22bdc9c09324bb807b07bbbad6169471
Jat15 <jinan15@hotmail.com>
2023-02-24 22:29:22 +0100

    [Router] Update
    - Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthComponent} from "./pages/auth/auth.component";
+import {MyPlanningsComponent} from "./pages/my-plannings/my-plannings.component";
+import {IsSignInGuard} from "./guards/is-sign-in.guard";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'my-plannings', component: MyPlanningsComponent, canActivate: [ IsSignInGuard ]},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: AuthComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

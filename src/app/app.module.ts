dc341e848d5f1a671746f1e7f38c7bd4ab04451f
Jat15 <jinan15@hotmail.com>
2023-02-24 15:15:08 +0100

    [Module] Update App
    - Add MyPlannings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AuthComponent } from './pages/auth/auth.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { MyPlanningsComponent } from './components/my-plannings/my-plannings.component';
 import {HttpRequestInterceptor} from "./Interceptor/http-request/http-request.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    MyPlanningsComponent
   ],
     imports: [
         BrowserModule,

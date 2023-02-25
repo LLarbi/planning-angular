9af3b52811c4c3dd38059a8e4f385b3a5f03dafd
Jat15 <jinan15@hotmail.com>
2023-02-25 16:59:07 +0100

    [Router] Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import {AuthComponent} from "./pages/auth/auth.component";
 import {MyPlanningsComponent} from "./pages/my-plannings/my-plannings.component";
 import {IsSignInGuard} from "./guards/is-sign-in.guard";
 import {InscriptionComponent} from "./pages/inscription/inscription.component";
+import {PlanningsSharedComponent} from "./pages/plannings-shared/plannings-shared.component";
 
 
 const routes: Routes = [
   {path: 'my-plannings', component: MyPlanningsComponent, canActivate: [IsSignInGuard]},
+  {path: 'plannings-shared', component: PlanningsSharedComponent, canActivate: [IsSignInGuard]},
   {path: 'login', component: AuthComponent},
   {path: 'inscription', component: InscriptionComponent},
   {path: '**', redirectTo: "/my-plannings"},

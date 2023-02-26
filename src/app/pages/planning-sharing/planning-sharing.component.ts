c4c96cf57793b15bdb07aaa33733eb6f9107bc04
Jat15 <jinan15@hotmail.com>
2023-02-26 22:13:01 +0100

    [Component] Update planning-sharing

---
+++ src/app/pages/planning-sharing/planning-sharing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planning-sharing',
+  templateUrl: './planning-sharing.component.html',
+  styleUrls: ['./planning-sharing.component.css']
+})
+export class PlanningSharingComponent {
+
+}
